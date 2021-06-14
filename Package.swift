3b08b3fb892ed4dba80f3be70fe065bf9c5e0202
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2021-06-14 16:00:22 -0700

    Beta Tealeaf build: 10_6_91
    Beta Tealeaf Change Notes:
    Enhancements
    
    - Library is now being built for xcframeworks with latest xcode 12.5.
    - Adding a flag SwiftUICaptureNonVariadic in TealeafAdvancedConfig.json to help capture all non Variadic pages from UIHostingController of SwiftUI. Default value is set to false.
    - Ability to turn off textChanged events to limit listening to user typing on textBox.
    
    Bug Fixes
    
    - After iOS 14 Apple changed NSURLSession private classes, developer need to use a completion handler and not the delegate in order to swizzle correctly for iOS 12 .x and below.
    - After iOS 14 Apple changed logic for images. We noticed that pngs that use transpaency where getting same id for normal replay which produced incorrect id values. Because Apple only using first layer of png to get image data.
    - When using Enhanced Replay, made adjustments on UIDatePicker to capture page without a flicker that would happen sometimes on certain devices.
    - Adding support for SwiftUI.
    - Fixed an issue with backbutton not capturing correct page on gesture event. I am using upper left zone to assume backbutton is selected to use correct page.
    - Adjusted capture of inner views of WKWebview which covered replay of webview and appeared grayish white.
    - Added a flag 'AddGestureRecognizerUIScrollView' in TealeafAdvancedConfig.json to help with not auto enabling shouldRecognizeSimultaneouslyWithGestureRecognizer on UIScrollView which breaks the layout when you have a UIPageControl with UIScrollView. When you swipe left or right, the table also scrolls due to enabling shouldRecognizeSimultaneouslyWithGestureRecognizer to return YES.
    - Removed 'UIWebview' text that was being flagged by Apple.
    
    Known Issues
    
    - Captures gesture even when the gestureRecognizer is set to return 'NO', this is an Apple defect.
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - The pink boarder on Gestures screenview is off when captured using ER, because if timing or garbage collection.
    - Template issue - The back link is missing during capture on some screens Progressindicator, AlertViewController, Gestures screenview and picker view.
    - Template issue - Battery icon not visible in green anymore.
    - Improvement :- 'connectionType' remains N/A if device is on WiFi on app start.
    - DoPostOnScreenChange is not working as expected, because of temp queue.
    - Reporting of multiple carrier names in JSON if the device has Dual SIM is a beta feature.
    - React Native has an issue for dynamic builds. We will use static version instead.
    - Certain Alerts may appear correctly during replay.
    - If a View Controller has more than one WKWebViews, gestures may be off by a few pixels
    - iPadOS Multi Window apps are not YET supported.

--- Package.swift
+++ Package.swift
@@ -30,8 +30,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Tealeaf",
-            url: "https://github.com/acoustic-analytics/Tealeaf/releases/download/10.6.90/Tealeaf_XCFramework_Debug.zip",
-            checksum: "b7edbe57906f4c6034d0b2ba88cfdf8828d49faf582d582e0b436884b39a51f7"),
+            url: "https://github.com/acoustic-analytics/Tealeaf/releases/download/10.6.91/Tealeaf_XCFramework_Debug.zip",
+            checksum: "cb4f19399166b4c27cb24304ce38d0c95f756aae89ae2d28275988cd4ef695ab"),
         .binaryTarget(
             name: "EOCore",
             url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.55/EOCore_XCFramework_Debug.zip",
