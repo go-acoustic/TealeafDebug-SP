68b31f7ac70eec5113415058bc97b02cc1d2a3f0
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2022-03-25 00:19:17 -0700

    Beta Tealeaf build: 10.6.160
    Beta Tealeaf Change Notes:
    Bug Fixes:
    
    - UIC queued data for WKWebview was being sent back in wrong order. Made an adjust to javascript script.
    - Update syntax due to swift issue with NSNumber during json creation.
    - Killswitch returns sometimes too slow and page displayed is not captured.
    - Gestures should not be captures on Webviews.
    
    Improvements:
    
    - New minimal os is 12.0 from 9.0 for library.
    - Correct name of page and get class name versus just getting UINavigationController.
    
    Our environment for this release:
    
    - Xcode 13.2.1
    - MacOS 11.6.1
    - iOS 12.x to 15.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues:
    
    - Captures gesture even when the gestureRecognizer is set to return 'NO', this is an Apple defect.
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - The pink boarder on Gestures screenview is off when captured using ER, because if timing or garbage collection.
    - Template issue - The back link is missing during capture on some screens Progressindicator, AlertViewController, Gestures screenview and picker view.
    - Template issue - Battery icon not visible in green anymore.
    - Improvement :- 'connectionType' remains N/A if device is on WiFi on app start.
    - DoPostOnScreenChange is not working as expected, because of temp queue.
    - Reporting of multiple carrier names in JSON if the device has Dual SIM is a beta feature.
    - React Native has an issue for dynamic builds. We will use static version instead.
    - Certain Alerts may appear correctly during replay.
    - If a View Controller has more than one WKWebViews, gestures may be off by a few pixels
    - iPadOS Multi Window apps are not YET supported.

--- Package.swift
+++ Package.swift
@@ -34,8 +34,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Tealeaf",
-            url: "https://github.com/acoustic-analytics/Tealeaf/releases/download/10.6.159/Tealeaf_XCFramework_Debug.zip",
-            checksum: "1b8226a3223f13ea0c1439f465fc1873e9685741869ca4d663ac680cc65ea186"),
+            url: "https://github.com/acoustic-analytics/Tealeaf/releases/download/10.6.160/Tealeaf_XCFramework_Debug.zip",
+            checksum: "b52356f7e0d4f9b1f0666ad93a292280d26441a16b6a986eff16f3e1d8ed53ce"),
         .binaryTarget(
             name: "EOCore",
             url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.199/EOCore_XCFramework_Debug.zip",
