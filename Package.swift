8001d84ea3b551880281c0371a45353fc3ef0168
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2022-06-09 18:42:32 -0700

    Beta EOCore build: 2.3.225
    Beta EOCore Change Notes:
    Bug Fixes
    
    - Update syntax due to swift issue with NSNumber during json creation.
    
    Improvements:
    
    - New minimal os is 12.0 from 9.0 for library.
    - Add more debug information what is being used for configuration settings.
    
    Our environment for this release:
    
    - Xcode 13.2.1
    - MacOS 11.6.1
    - iOS 12.x to 15.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
             checksum: "f1ce5636cfe80c3e0664697762624338df98450ad16f67d03e11ec19fdadad27"),
         .binaryTarget(
             name: "EOCore",
-            url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.224/EOCore_XCFramework_Debug.zip",
-            checksum: "6b9b1e8d5bad0146091bb09dad6e222225103d56f234dbdbba243f8be114125b"),
+            url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.225/EOCore_XCFramework_Debug.zip",
+            checksum: "a860ba31b7b9ea105bfadac483d619c0ab18de4a520d9d698ce35ecbdcb84335"),
     ]
 )
