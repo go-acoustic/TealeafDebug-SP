86725bfc2776f79f9f287a8c305e002f9b086f5f
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2022-09-01 11:22:44 -0700

    Beta Tealeaf build: 10.6.217
    Beta Tealeaf Change Notes:
    Bug Fixes:
    
    - Adjust to disable library if no capture is needed. There was an issue with webviews.
    - Adjust for wkwebview id creation before being added to layout. This fixes issues with dom data not matching webview id from type 10 data for replay.
    
    Improvements:
    
    - None.
    
    Our environment for this release:
    
    - Xcode 13.4
    - MacOS 12.4
    - iOS 12.x to 15.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues:
    
    - Captures gesture even when the gestureRecognizer is set to return 'NO', this is an Apple defect.
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - The pink boarder on Gestures screenview is off when captured using ER, because if timing or garbage collection.
    - Template issue - The back link is missing during capture on some screens Progressindicator, AlertViewController, Gestures screenview and picker view.
    - Template issue - Battery icon not visible in green anymore.
    - Improvement :- 'connectionType' remains N/A if device is on WiFi on app start.
    - DoPostOnScreenChange is not working as expected, because of temp queue.
    - Reporting of multiple carrier names in JSON if the device has Dual SIM is a beta feature.
    - React Native has an issue for dynamic builds. We will use static version instead.
    - Certain Alerts may appear correctly during replay.
    - If a View Controller has more than one WKWebViews, gestures may be off by a few pixels
    - iPadOS Multi Window apps are not YET supported.

--- Package.swift
+++ Package.swift
@@ -34,8 +34,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Tealeaf",
-            url: "https://github.com/acoustic-analytics/Tealeaf/releases/download/10.6.216/Tealeaf_XCFramework_Debug.zip",
-            checksum: "2f2ca05f0ba625e880065eb2aeab46a2879e2ac806f775f9cc4420c6dde30ab4"),
+            url: "https://github.com/acoustic-analytics/Tealeaf/releases/download/10.6.217/Tealeaf_XCFramework_Debug.zip",
+            checksum: "96274146f7c1bbc421d95f469d74dc2d1c8cdd1aa384e3e0855e2b344a93c60a"),
         .binaryTarget(
             name: "EOCore",
             url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.245/EOCore_XCFramework_Debug.zip",
