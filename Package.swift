491a4430697f19ef3b5c4b183d86df07c1615a4b
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2024-02-20 20:06:37 -0800

    Beta EOCore build: 2.3.269
    Beta EOCore Change Notes:
    Bug Fixes:
    
    - None
    
    Improvements:
    
    - Add PrivacyInfo.xcprivacy and sign xcframeworks.
    
    Our environment for this release:
    
    - Xcode 15.2
    - MacOS 14.3.1
    - iOS 12.x to 17.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //
-// Copyright (C) 2023 Acoustic, L.P. All rights reserved.
+// Copyright (C) 2024 Acoustic, L.P. All rights reserved.
 //
 // NOTICE: This file contains material that is confidential and proprietary to
 // Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
@@ -38,7 +38,7 @@ let package = Package(
             checksum: "ec83bb6e5547562020ad9cbe22e20efd5a2088bcde912419ffa6083748702f17"),
         .binaryTarget(
             name: "EOCore",
-            url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.268/EOCore_XCFramework_Debug.zip",
-            checksum: "b5e6abdb07168712b371c89e5c6b3b2f94d4b27fba3bb613112878c855c7aecf"),
+            url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.269/EOCore_XCFramework_Debug.zip",
+            checksum: "759a90149204575ee63cc8c809082867bb1c334375f5039ebf8b900a5c81e3fb"),
     ]
 )
