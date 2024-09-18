b7cfaa798ba0827cfb5a17492d30709c68769ed1
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2024-09-17 19:40:19 -0700

    Beta EOCore build: 2.3.277
    Beta EOCore Change Notes:
    Bug Fixes:
    
    - None
    
    Improvements:
    
    - When flush is executed in EOCore, it will display if payload is not empty.
    
    Our environment for this release:
    
    - Xcode 15.4
    - MacOS 14.5
    - iOS 12.x to 17.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
             checksum: "6677a6f3146b736c75f77fd9697ef29ed75d5d59074621c09feb829794e34301"),
         .binaryTarget(
             name: "EOCore",
-            url: "https://github.com/go-acoustic/EOCore/releases/download/2.3.276/EOCore_XCFramework_Debug.zip",
-            checksum: "736c96ad8f7a22354456f4183a1324ea9d07209ff72884a42d86cef642f60b76"),
+            url: "https://github.com/go-acoustic/EOCore/releases/download/2.3.277/EOCore_XCFramework_Debug.zip",
+            checksum: "84043da51a582c68e75f8b21af2ef011c62cafc17f50b0152d0d68ce5f9aba5f"),
     ]
 )
