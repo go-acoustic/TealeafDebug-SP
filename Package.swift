5a3bebbd5c0a63a3eb06753ff51723affb6c67de
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2025-01-22 16:09:12 -0800

    Beta Tealeaf build: 10.6.318
    Beta Tealeaf Change Notes:
    Bug Fixes:
    
    - None.
    
    Improvements:
    
    - None.
    
    Our environment for this release:
    
    - Xcode 16.0
    - MacOS 15.0
    - iOS 13.x to 18.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues:
    
    - Captures gesture even when the gestureRecognizer is set to return 'NO', this is an Apple defect.
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - The pink boarder on Gestures screenview is off when captured using ER, because if timing or garbage collection.
    - Template issue - The back link is missing during capture on some screens Progressindicator, AlertViewController, Gestures screenview and picker view.
    - Template issue - Battery icon not visible in green anymore.
    - Improvement :- 'connectionType' remains N/A if device is on WiFi on app start.
    - DoPostOnScreenChange is not working as expected, because of temp queue.
    - Reporting of multiple carrier names in JSON if the device has Dual SIM is a beta feature.
    - React Native has an issue for dynamic builds. We will use static version instead.
    - Certain Alerts may appear correctly during replay.
    - If a View Controller has more than one WKWebViews, gestures may be off by a few pixels
    - iPadOS Multi Window apps are not YET supported.

--- Package.swift
+++ Package.swift
@@ -34,8 +34,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Tealeaf",
-            url: "https://github.com/go-acoustic/Tealeaf/releases/download/10.6.317/Tealeaf_XCFramework_Debug.zip",
-            checksum: "41d492131671398c84f4e17070fe9397cb436672864d3f2d97faa48a60418725"),
+            url: "https://github.com/go-acoustic/Tealeaf/releases/download/10.6.318/Tealeaf_XCFramework_Debug.zip",
+            checksum: "5dac3521312a98d25eeb1f12f4dd3c215d7e292d902a5966f04ef2e2867adf93"),
         .binaryTarget(
             name: "EOCore",
             url: "https://github.com/go-acoustic/EOCore/releases/download/2.3.312/EOCore_XCFramework_Debug.zip",
