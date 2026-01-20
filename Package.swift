3ffe442035e9d595b65bba679a589ec879cb04e6
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2026-01-20 01:59:39 -0800

    Beta EOCore build: 2.3.330
    Beta EOCore Change Notes:
    Bug Fixes:
    
    - Default to system value for User-Agent header if it is not available
    
    Improvements:
    
    - None.
    
    Our environment for this release:
    
    - Xcode 26.0.1
    - MacOS 15.6.1
    - iOS 15.1 to 26.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
             checksum: "e1a06188aeec5a12bfbe9a4f9ab0428771c07001f5b69533cf710c0ad60070b8"),
         .binaryTarget(
             name: "EOCore",
-            url: "https://github.com/go-acoustic/EOCore/releases/download/2.3.329/EOCore_XCFramework_Debug.zip",
-            checksum: "e0b6c3b5ed85c055b726176f22048b79f2a47671281cb093730f8b3bcc5397a3"),
+            url: "https://github.com/go-acoustic/EOCore/releases/download/2.3.330/EOCore_XCFramework_Debug.zip",
+            checksum: "6dca507bcfac1bd5024b8ce8a491a4ad7c3a94e4a02acfdfc2a130d8c11efc30"),
     ]
 )
